ba502744a701156031b554c3e3ecd8a4fa2c837c
Jacques Nel <jmnel92@gmail.com>
2019-10-24 01:26:28 -0400

    Created setup.py

---
+++ examples/foo.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+a = os.path.realpath(__file__)
+print(a)
+#sys.path.insert(1, '
+
+#from bactrian import Variable
+
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+        name='bactrian',
+        version='0.0.1',
+        description='Modeling language library that emulates Dromedary Studio\'s API',
+        url='git@github.com:jmnel/bactrian.git',
+        author='Jacques Nel, Askhan Moatamed',
+        author_email='jmnel92@gmail.com',
+        license='unlicense',
+        packages=['bactrian'],
+        zip_safe=False
+        )
+
